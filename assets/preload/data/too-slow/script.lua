0599e54d0e9700bf7f9af1878c47fc9db340ee7a
EnzoXD2 <96305131+EnzoXD2@users.noreply.github.com>
2022-12-12 10:40:07 -0300

    Add files via upload

--- assets/preload/data/too-slow/script.lua
+++ assets/preload/data/too-slow/script.lua
@@ -1,3 +1,19 @@
 function onCreate()
 	setProperty('cameraSpeed', getProperty('cameraSpeed') + 1)
 end
+
+function onBeatHit()
+   if curBeat == 95 then
+        doTweenAlpha('game', 'camGame', 0, 0.5, 'linear')
+        doTweenAlpha('icon', 'iconP2', 0, 0.5, 'linear')
+end
+   if curBeat == 111 then
+        doTweenAlpha('game2', 'camGame', 1, 0.5, 'linear')
+        doTweenAlpha('icon2', 'iconP2', 1, 0.5, 'linear')
+        doTweenColor('timeColor', 'timeTxt', 'E455D8', 1, 'linear')
+        doTweenColor('timeColor2', 'timeBar', 'E455D8', 1, 'linear')
+        doTweenColor('timeColor3', 'timeBarBG', 'E455D8', 1, 'linear')
+        removeLuaSprite('main lol', false)
+        setProperty('main dead.alpha', 1)
+      end
+end
